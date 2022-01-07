a230c8bcf1e8205850fc1379373bca09328ca8c5
TotpalIstvan <totpal.istvan@petrik.hu>
2022-01-07 20:32:42 +0100

    bootstrap update && kezedleges form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
       
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Felhasználónév:</Form.Label>
+    <Form.Control type="text" placeholder="Írj be egy felhasználó nevet!" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email cím:</Form.Label>
+    <Form.Control type="email" placeholder="Írj be egy email címet!" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Jelszó:</Form.Label>
+    <Form.Control type="password" placeholder="Írj be egy jelszót!" />
+  </Form.Group>
+  
+  <Button variant="primary" type="submit">
+    Bejelentkezés
+  </Button>
+</Form>
       
-      </header>
     </div>
   );
 }
