4dc42f62ee1a4a594ffb504ac05cb0717d66300d
yournamem <email@example.com>
2022-03-22 13:04:19 +0100

    Táblázat megkezdve

--- src/components/websites/Leaderboard.js
+++ src/components/websites/Leaderboard.js
@@ -2,11 +2,35 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 export function Leaderboard()  {
 
     return(
 <div className='Leaderboard'>
- 
+ <table>  
+<tr>
+    <th>
+        Név
+    </th>
+    <th>
+        Pontszám
+    </th>
+    <th>
+        Helyezés
+    </th>
+</tr>
+<tr>
+    <td>
+        János
+    </td>
+    <td>
+        100
+    </td>
+    <td>
+        1.
+    </td>
+</tr>
+ </table>
 
 </div>
     );
