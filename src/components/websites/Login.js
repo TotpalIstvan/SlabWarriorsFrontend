f3a2415d67d95c48a84bd18a31f6e545cff2aaf6
TotpalIstvan <totpal.istvan@petrik.hu>
2022-04-05 20:10:08 +0200

    styling

--- src/components/websites/Login.js
+++ src/components/websites/Login.js
@@ -15,10 +15,10 @@ export function Login()  {
   const [email,setEmail]=useState("");
 
   async function signUp() {
-    let item = {name, password, email};
+    let item = {name, email, password};
     console.warn(item);
 
-  let result = await fetch("http://localhost:8000/api/users/login", 
+  let result = await fetch("http://127.0.0.1:8000/api/users/login", 
     {
       method: "POST",
       body: JSON.stringify(item),
@@ -36,16 +36,16 @@ export function Login()  {
 <div className='Login'>
   
 <Form id='LoginForm'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="username">
     <Form.Label>Username:</Form.Label>
     <Form.Control type="text"   onChange={(e) => setName(e.target.value)} value={name}/>
   </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
+  <Form.Group className="mb-3" controlId="email">
     <Form.Label>Email address:</Form.Label>
     <Form.Control type="email"  onChange={(e) => setEmail(e.target.value)} value={email}/>
   </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
+  <Form.Group className="mb-3" controlId="password">
     <Form.Label>Password:</Form.Label>
     <Form.Control type="password"   onChange={(e) => setPassword(e.target.value)} value={password}/>
   </Form.Group> 
--- src/components/websites/Registration.js
+++ src/components/websites/Registration.js
@@ -3,40 +3,22 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Registration.scss';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
 
 
 
-<<<<<<< HEAD
-const fields = Yup.object().shape({
-  name: Yup.string().required(),
-  email: Yup.string().email().required(),
-  password: Yup.string().required().min(8).max(50),
-  password_confirm: Yup.string().oneOf([Yup.ref('password'),null])
-});
-=======
->>>>>>> 13f75dcd4e61f0594810ad8d920125c68afac1fa
+
 
 export function Registration()  {
-
-
-
 
   const [name,setName]=useState("");
   const [email,setEmail]=useState("");
   const [password,setPassword]=useState("");
-<<<<<<< HEAD
   const [password_confirm, setPasswordConfirm] = useState("");
-=======
-  const [password_confirm,setPasswordConfirmation]=useState("");
-
->>>>>>> 13f75dcd4e61f0594810ad8d920125c68afac1fa
   async function signUp() {
     let item = {name, email, password, password_confirm};
     console.warn(item);
 
-  let result = await fetch("http://localhost:8000/api/users/register", 
+  let result = await fetch("http://127.0.0.1:8000/api/users/register", 
     {
       method: "POST",
       body: JSON.stringify(item),
@@ -53,46 +35,34 @@ export function Registration()  {
 
 
     return(
-<<<<<<< HEAD
     
 
 
-<Form id='RegisterForm' noValidate onSubmit={handleSubmit}>
-=======
-<div className='Registration'>
-  
 <Form id='RegisterForm'>
-      <Form.Group className="mb-3" controlId="Name">
->>>>>>> 13f75dcd4e61f0594810ad8d920125c68afac1fa
 
       <Form.Group className="mb-3" controlId="UsernameField" required>
     <Form.Label className='labelok' required isinvalid="true">Username:</Form.Label>
-    <Form.Control type="text" placeholder="Write a username!" value={name}  onChange={(e) => setName(e.target.value)}  isValid={touched.email && !errors.name}/>
+    <Form.Control type="text" placeholder="Write a username!" value={name}  onChange={(e) => setName(e.target.value)} />
   </Form.Group>
-<<<<<<< HEAD
     <Form.Control.Feedback type="invalid">
-    {errors.name}
+    
   </Form.Control.Feedback>
 
   <Form.Group className="mb-3" controlId="EmailField" required>
-=======
-  <Form.Group className="mb-3" controlId="Email">
->>>>>>> 13f75dcd4e61f0594810ad8d920125c68afac1fa
     <Form.Label className='labelok'>Email address:</Form.Label>
-    <Form.Control type="email" placeholder="write an email!" value={email}    onChange={(e) => setEmail(e.target.value)} isValid={touched.email &&!errors.email}/>
+    <Form.Control type="email" placeholder="write an email!" value={email}    onChange={(e) => setEmail(e.target.value)}/>
     <Form.Control.Feedback type="invalid">
-    {errors.email}
+   
   </Form.Control.Feedback>
   </Form.Group>
 
-<<<<<<< HEAD
 
  
   <Form.Group className="mb-3" controlId="PasswordField" required>
     <Form.Label className='labelok'>Password:</Form.Label>
-    <Form.Control type="password" placeholder="write a password!"   onChange={(e) => setPassword(e.target.value)} value={password} isValid={touched.password && !errors.password}/>
+    <Form.Control type="password" placeholder="write a password!"   onChange={(e) => setPassword(e.target.value)} value={password}/>
     <Form.Control.Feedback type="invalid">
-   {errors.password}
+  
   </Form.Control.Feedback>
   </Form.Group>
 
@@ -100,16 +70,7 @@ export function Registration()  {
  
   <Form.Group className="mb-3" controlId="PasswordConfirmField" required>
     <Form.Label className='labelok'>Password Confirmation:</Form.Label>
-    <Form.Control type="password" placeholder="write your password again!"   onChange={(e) => setPasswordConfirm(e.target.value)} value={password_confirm} isValid={touched.password_confirm && !errors.password_confirm}/>
-=======
-  <Form.Group className="mb-3" controlId="Password">
-    <Form.Label className='labelok'>Password:</Form.Label>
-    <Form.Control type="password" placeholder="write a password!"   onChange={(e) =>setPassword(e.target.value)} value={password}/>
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="PasswordConfirm">
-    <Form.Label className='labelok'>Password Confirmation:</Form.Label>
-    <Form.Control type="password" placeholder="write your password again!"   onChange={(e) => setPasswordConfirmation(e.target.value)} value={password_confirm}/>
->>>>>>> 13f75dcd4e61f0594810ad8d920125c68afac1fa
+    <Form.Control type="password" placeholder="write your password again!"   onChange={(e) => setPasswordConfirm(e.target.value)} value={password_confirm}/>
   </Form.Group>
  
   
