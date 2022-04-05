32f18f4076ac60e83810eefa087bdd8e236380bd
TotpalIstvan <totpal.istvan@petrik.hu>
2022-04-06 00:25:01 +0200

    id hozzáadása

--- src/components/websites/Leaderboard.js
+++ src/components/websites/Leaderboard.js
@@ -1,92 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useTable } from 'react-table';
 
 
 export function Leaderboard()  {
-    const data = React.useMemo(
-        () => [
-          {
-            col1: 'Hello',
-            col2: 'World',
-          },
-          {
-            col1: 'react-table',
-            col2: 'rocks',
-          },
-          {
-            col1: 'whatever',
-            col2: 'you want',
-          },
-        ],
-        []
-      )
-    
-      const columns = React.useMemo(
-        () => [
-          {
-            Header: 'ID',
-            accessor: 'col1', // accessor is the "key" in the data
-          },
-          {
-            Header: 'Név',
-            accessor: 'col2',
-          },
-        ],
-        []
-      )
-    
-      const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-      } = useTable({ columns, data })
     return(
 <div className='Leaderboard'>  
- <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+ <table id='leaderboardtable'>
        <thead>
-         {headerGroups.map(headerGroup => (
-           <tr {...headerGroup.getHeaderGroupProps()}>
-             {headerGroup.headers.map(column => (
-               <th
-                 {...column.getHeaderProps()}
-                 style={{
-                   borderBottom: 'solid 3px red',
-                   background: 'aliceblue',
-                   color: 'black',
-                   fontWeight: 'bold',
-                 }}
-               >
-                 {column.render('Header')}
-               </th>
-             ))}
-           </tr>
-         ))}
        </thead>
-       <tbody {...getTableBodyProps()}>
-         {rows.map(row => {
-           prepareRow(row)
-           return (
-             <tr {...row.getRowProps()}>
-               {row.cells.map(cell => {
-                 return (
-                   <td
-                     {...cell.getCellProps()}
-                     style={{
-                       padding: '10px',
-                       border: 'solid 1px gray',
-                       background: 'papayawhip',
-                     }}
-                   >
-                     {cell.render('Cell')}
-                   </td>
-                 )
-               })}
-             </tr>
-           )
-         })}
+       <tbody>
        </tbody>
      </table>
      </div>
