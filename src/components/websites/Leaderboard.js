f5f76bcd4ae870ecd82f53d3bbd00b08b2f59a90
TotpalIstvan <totpal.istvan@petrik.hu>
2022-04-24 14:45:28 +0200

    változtatások

--- src/components/websites/Leaderboard.js
+++ src/components/websites/Leaderboard.js
@@ -12,15 +12,15 @@ class Leaderboard extends Component {
     const response = await axios.get('http://localhost:8000/api/users')
     .catch(function (error) {
       if (error.response) {
-        // Request made and server responded
+        
         console.log(error.response.data);
         console.log(error.response.status);
         console.log(error.response.headers);
       } else if (error.request) {
-        // The request was made but no response was received
+        
         console.log(error.request);
       } else {
-        // Something happened in setting up the request that triggered an Error
+        
         console.log('Error', error.message);
       }
   ;
